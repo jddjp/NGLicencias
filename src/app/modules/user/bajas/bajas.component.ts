cde96d19a947881809a38fba4cde753de29d5166
12Juansilva <simjd1401as@gmail.com>
2022-10-11 18:12:25 -0500

    baj

--- src/app/modules/user/bajas/bajas.component.ts
+++ src/app/modules/user/bajas/bajas.component.ts
@@ -1,137 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
-import { ConfirmationService } from 'primeng/api';
-import { ContactoModel } from 'src/app/models/contacto.model';
-import { ContactoService } from 'src/app/services/contacto.service';
-import { ExcelService } from 'src/app/services/excel.service';
-import { AltasComponent } from '../altas/altas.component';
 import { DocumentData, QuerySnapshot } from 'firebase/firestore';
+import { ConvocatoriasService } from 'src/app/services/convocatoria.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
+import * as XLSX from 'xlsx';
+import { ExcelService } from 'src/app/services/excel.service';
+import { ConfirmationService, MessageService } from 'primeng/api';
 @Component({
   selector: 'app-bajas',
   templateUrl: './bajas.component.html',
   styleUrls: ['./bajas.component.css']
 })
 export class BajasComponent implements OnInit {
-  contactomodel = {
+  convocatoria = {
     nombre: '',
-    app: '',
-    apm: '',
-    fechaNa: '',
-    calle : '',
-    colonia: '',
-    codigoPostal: '',
-    noInterior: '',
-    estado: '',
-    municipio: '',
-    estadocivil: '',
-    correoElectro: '',
     numeroTelefonico: '',
-    nombreInstiticion: '',
-    dirrecciónInstitucion: '',
-    calleinstitucion: '',
-    coloniadesInstiticion: '',
-    códigopostalInstiticion: '',
-    noInteriorInstitucion: '',
-    estadoInstitucion: '',
-    MunicipioInstitucion: '',
-    aniodeIngreso: '',
-    aniodeEgreso: '',
-    aniodeEstudios: '',
-    recibióCertificado: '',
-    nombreEscSecundaria: '',
-    direccionEscSecundaria: '',
-    calleEscSecundaria: '',
-    coloniaEscSecundaria: '',
-    codigopostalEscSecundaria: '',
-    noInteriorEscSecundaria: '',
-    estadoEscSecundaria: '',
-    municipioEscSecundaria: '',
-    aniodeIngresoEscSecundaria: '',
-    aniodeEgresoEscSecundaria: '',
-    aniosdeEstudioEscSecundaria: '',
-    recibioCertificadoEscSecundaria: '',
-    nombreInstitucionPreparatoria: '',
-    direccionInstitucionPreparatoria: '',
-    callePreparatoria: '',
-    coloniaPreparatoria: '',
-    codigopostalPreparatoria: '',
-    noInteriorPreparatoria: '',
-    estadoPreparatoria: '',
-    municipioPreparatoria: '',
-    aniodeIngresoPreparatoria: '',
-    aniodeEgresoPreparatoria: '',
-    aniosdeEstudioPreparatoria: '',
-    recibioCertificadoPreparatoria: '',
-    nombreInstitucionSuperior: '',
-    direccionInstitucionSuperior: '',
-    calleSuperior: '',
-    coloniaSuperior: '',
-    codigopostalSuperior: '',
-    noInteriorSuperior: '',
-    estadoSuperior: '',
-    municipioSuperior: '',
-    aniodeIngresoSuperior: '',
-    aniodeEgresoSuperior: '',
-    aniosdeEstudioSuperior: '',
-    recibioCertificadoSuperior: '',
-    nombreInstitucionMaestria: '',
-    direccionInstitucionMaestria: '',
-    calleMaestria: '',
-    coloniaMaestria: '',
-    codigopostalMaestria: '',
-    noInteriorMaestria: '',
-    estadoMaestria: '',
-    municipioMaestria: '',
-    aniodeIngresoMaestria: '',
-    aniodeEgresoMaestria: '',
-    aniosdeEstudioMaestria: '',
-    recibioCertificadoMaestria: '',
-    nombreInstitucionDoctorado: '',
-    direccionInstitucionDoctorado: '',
-    calleDoctorado: '',
-    coloniaDoctorado: '',
-    aniodeIngresoDoctorado: '',
-    aniodeEgresoDoctorado: '',
-    aniosdeEstudioDoctorado: '',
-    recibioCertificadoDoctorado: '',
-    categoriaSalud: '',
-    especifiquePadecimiento: '',
-    puestoTrabajo: '',
-    numeroEmpleado: '',
-    noAnioenelpuesto: '',
-    puestosAnteriores: '',
-    departamentooArea: '',
-    aptitudesCH: '',
-    historial: '',
-    nombreInstitucion:'',
-    direccionInstitucion:'',
-    codigopostalInstitucion:'',
-    estadoinstitucion:'',
-    aniodeEstudioEscSecundaria:'',
-    aniodeEstudioPreparatoria:'',
-    aniodeEstudioSuperior:'',
-    aniodeEstudioMaestria:'',
-    aniodeEstudioDoctorado:'',
-    coloniainstitucion:'',
-    correoElectronico:''
+    fechaFin: ''
   }
-  contactosCollectiondata: { id: string, titulo: string, fechaInicio: Date, fechaFin: Date }[] | any = [];
+
+  convocatoriaCollectiondata: { id: string, nombre: string, numeroTelefonico: Date, fechaFin: Date }[] | any = [];
   convocatoriaForm: FormGroup;
   submitted: boolean;
 
   visible: boolean;
- 
-  altasUsuariosModel: any;
+  convocatoriaModelDialog: boolean;
+  convocatoriaModel: any;
+
 visibleDe:boolean = false
 id: any;
   constructor(
-
-    private contactoService: ContactoService,
-    private toastr: ToastrService,
-    private firebaseService: ContactoService,
+    private firebaseService: ConvocatoriasService,
     private fb: FormBuilder,
+    private toastr: ToastrService,
     private exporExcel: ExcelService,
     private confirmationService: ConfirmationService,
     ) {
@@ -144,7 +45,7 @@ id: any;
 
     this.get();
     this.firebaseService.obsr_UpdatedSnapshot.subscribe((snapshot) => {
-      this.updatecontactoCollection(snapshot);
+      this.updateConvocatoriaCollection(snapshot);
     })
   }
 
@@ -161,16 +62,16 @@ id: any;
     this.submitted = true;
     // this.visible = false
     // if (this.convocatoriaForm.valid) {
-      if (this.altasUsuariosModel.nombre.trim() && this.altasUsuariosModel.app.trim() && this.altasUsuariosModel.apm.trim()) {
-        if (this.altasUsuariosModel.id) {
-          this.firebaseService.updatecontacto(this.altasUsuariosModel.nombre, this.altasUsuariosModel.app, this.altasUsuariosModel.apm, this.altasUsuariosModel.id)
+      if (this.convocatoriaModel.titulo.trim() && this.convocatoriaModel.fechaFin.trim() && this.convocatoriaModel.fechaInicio.trim()) {
+        if (this.convocatoriaModel.id) {
+          this.firebaseService.updateConvocatoria(this.convocatoriaModel.titulo, this.convocatoriaModel.fechaInicio, this.convocatoriaModel.fechaFin, this.convocatoriaModel.id)
           this.visible = false
          
           
         } else {
           
-          const { nombre, app, apm} = this.altasUsuariosModel;
-          await this.firebaseService.addcontacto(nombre, app, apm);
+          const { titulo, fechaInicio, fechaFin } = this.convocatoriaModel;
+          await this.firebaseService.addConvocatoria(titulo, fechaInicio, fechaFin);
           this.convocatoriaForm.reset()
 this.visible = false
 this.submitted = false
@@ -191,18 +92,15 @@ this.submitted = false
   }
 
   async get() {
-    const snapshot = await this.firebaseService.getcontactos();
-    newFunction();
-
-    function newFunction() {
-      this.updatecontactoCollection(snapshot);
-    }
+    const snapshot = await this.firebaseService.getConvocatoria();
+    this.updateConvocatoriaCollection(snapshot);
+    console.log(this.updateConvocatoriaCollection);
   }
 
-  updatecontactoCollection(snapshot: QuerySnapshot<DocumentData>) {
-    this.contactosCollectiondata = [];
+  updateConvocatoriaCollection(snapshot: QuerySnapshot<DocumentData>) {
+    this.convocatoriaCollectiondata = [];
     snapshot.docs.forEach((student) => {
-      this.contactosCollectiondata.push({ ...student.data(), id: student.id });
+      this.convocatoriaCollectiondata.push({ ...student.data(), id: student.id });
     })
   }
 
@@ -212,15 +110,15 @@ this.submitted = false
       header: 'Confirmacion',
       icon: 'pi pi-exclamation-triangle',
       accept: () => {
-          this.firebaseService.deletecontacto(docId.id);
+          this.firebaseService.deleteConvocatoria(docId.id);
       }
   });
   }
 
   edit: boolean = false;
-  editar(contactomodel: any) {
+  editar(convocatoria: any) {
     this.visible = true
-    this.altasUsuariosModel= { ...contactomodel }
+    this.convocatoriaModel = { ...convocatoria }
 
   }
   update(id: any) {
@@ -230,12 +128,12 @@ this.submitted = false
 
 
   Excel() {
-    this.exporExcel.convoc(this.contactosCollectiondata)
+    this.exporExcel.convoc(this.convocatoriaCollectiondata)
 
   }
 
   openNew() {
-    this. altasUsuariosModel= { nombre: '', app: '', apm: '' }
+    this.convocatoriaModel = { titulo: '', fechaInicio: '', fechaFin: '' }
     this.visible = true;
     
   }
