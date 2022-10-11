341b48d4d40b64c1b8ae004213ba3e23a50c2fb4
Jose Daniel <dejesusperezjosedaniel@gmail.com>
2022-10-11 02:02:26 -0500

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,21 +10,9 @@ const routes: Routes = [
     loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule),
     //canLoad: [AuthGuard]
   },
-  {
-    path: 'admin',
-    loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule),
+  
     //canLoad: [AuthGuard]
-  },
-  {
-    path: 'reedevento',
-    loadChildren: () => import('./modules/reedevento/reedevento.module').then(m => m.ReedEventoModule),
-    //canLoad: [AuthGuard]
-  },
-  {
-    path: 'reedeventoadmin',
-    loadChildren: () => import('./modules/reedeventoadmin/reedeventoadmin.module').then(m => m.ReedEventoAdminModule),
-    //canLoad: [AuthGuard]
-  },
+  
   { path: 'not-found', component: NotFoundComponent },
   { path: '**', redirectTo: 'portal' }
 ];
